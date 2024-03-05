daecdd45aa987443ad48c061534613b115974b5a
christanlinsangan <cmlinsangan.wup@gmail.com>
2024-03-05 15:57:26 +0800

    Added register html and register js

---
+++ register.js
@@ -0,0 +1,33 @@
+console.log("Hello World")
+console.error("Error Error Error")
+console.warn("Warning MERONG WARNINGG")
+
+
+// Variables 
+// string = words
+// number = 123, 434e, 23.54
+// boolean = true, false
+// object = ["apple", "banana"], [1,2,3,4], ["apple",3, true]
+
+//typeof
+// console.log(typeof("Sample lang po"))
+// Declaration of Variables
+//let name
+//let age
+// let isStudent
+// console.log(name,age,student)
+//console.table
+//let hobbies [watch tv, watch tae]
+let name = "Cholo";
+let lastname = "Linsangan"
+let age = "19";
+let isStudent = true;
+let hobbies = ["Gaming", "Eating"]
+run = function () {
+    console.log("Im Running✔");
+}
+console.log(isStudent, name, age, hobbies);
+console.log(typeof run);
+console.log("My name is" + name + " " + lastname);
+console.log(`My name is ${name} ${lastname}`);
+console.table(hobbies);
